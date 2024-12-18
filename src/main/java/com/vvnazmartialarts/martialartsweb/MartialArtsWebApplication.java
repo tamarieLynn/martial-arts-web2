7c2f163e05c5679fdc5287585e14e988f732a2a9
Tcarri <133730561+Tcarri@users.noreply.github.com>
2024-12-18 12:21:36 -0800

    Initial commit

---
+++ src/main/java/com/vvnazmartialarts/martialartsweb/MartialArtsWebApplication.java
@@ -0,0 +1,13 @@
+package com.vvnazmartialarts.martialartsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartialArtsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MartialArtsWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vvnazmartialarts/martialartsweb/MartialArtsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvnazmartialarts.martialartsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartialArtsWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
